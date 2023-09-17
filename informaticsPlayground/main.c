f3ee95757435b4c50f8d6f9cf8aa851b4d91a153
Alexey Shesterin <xenophorus@gmail.com>
2023-09-18 02:08:36 +1000

    123

--- informaticsPlayground/main.c
+++ informaticsPlayground/main.c
@@ -1,68 +1,32 @@
-//#include <stdio.h>
-//
-//int main() {
-//    printf("Hello, World!\n");
-//    return 0;
-//}
-
-
 #include <stdio.h>
-#include <stdlib.h>
-
-void f() {
-    char phrase[] = "My first program\n";
-    printf("%s", phrase);
-}
-
-int factorial(int n) {
-    if (n <= 1) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
-}
-
-int looper(int counter, int y, int acc) {
-    if (counter == 0) {
-        return acc;
-    } else {
-        return looper(counter - 1, acc, acc + y);
-    }
-}
-
-int fib(int n) {
-    return looper(n - 1, 1, 0);
-}
-
-//int main(int argc, char **argv) {
-////    system("chcp 1251");
-//    int num;
-//    num = 2;
-//    f();
-//    int a = factorial(7);
-//    printf("%d\n", a);
-//    int fn1 = fib(10);
-//    printf("%d\n", fn1); /*
-//    for (int i = 0; i < 10; ++i) {
-//        printf("%d\n", i * 2);
-//
-//    }*/
-//    return 0;
-//}
 
 int main(void) {
 
-    char line[] = "_________________________________________________\n";
-    char stars[] = "********_________________________________________\n";
-
-    for (int i = 0; i < 13; ++i) {
-        if ((i > 0 ) && (i < 6)) {
-            printf(stars);
+    int a = 27;
+    printf("%d ", a);
+    while (a != 1) {
+        if (a % 2 == 0) {
+            a = a / 2;
         } else {
-            printf(line);
+            a = a * 3 + 1;
+        }
+        printf("%d ", a);
+    }
+    printf("\n\n");
+    for (int i = 31; i < 128; ++i) {
+        if (i % 10 == 0) {
+            printf("%7d - %c\n", i, i);
+        } else {
+            printf("%7d - %c", i, i);
         }
     }
-
+    printf("\n\n");
+    for (int i = 1; i < 10; ++i) {
+        for (int j = 1; j < 10; ++j) {
+            printf("%4d", i * j);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
