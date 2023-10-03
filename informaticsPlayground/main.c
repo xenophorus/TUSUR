6ae6ad9750f84b4edbdebf6d4ef992b30639d6ca
Alexey Shesterin <xenophorus@gmail.com>
2023-10-03 22:32:54 +1000

    1

--- informaticsPlayground/main.c
+++ informaticsPlayground/main.c
@@ -1,31 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) {
-
-    int a = 27;
-    printf("%d ", a);
-    while (a != 1) {
-        if (a % 2 == 0) {
-            a = a / 2;
-        } else {
-            a = a * 3 + 1;
-        }
-        printf("%d ", a);
-    }
-    printf("\n\n");
-    for (int i = 31; i < 128; ++i) {
+    srand(time(0));
+    for (int i = 1; i <= 50; ++i) {
+        printf("%5d", rand() % 50);
         if (i % 10 == 0) {
-            printf("%7d - %c\n", i, i);
-        } else {
-            printf("%7d - %c", i, i);
+            printf("\n");
         }
-    }
-    printf("\n\n");
-    for (int i = 1; i < 10; ++i) {
-        for (int j = 1; j < 10; ++j) {
-            printf("%4d", i * j);
-        }
-        printf("\n");
     }
 
     return 0;
