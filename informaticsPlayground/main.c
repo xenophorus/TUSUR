23dc03919cc8bf50e7606bd699c0cbbd6c62575a
Alexey Shesterin <xenophorus@gmail.com>
2023-10-17 00:51:54 +1000

    1

--- informaticsPlayground/main.c
+++ informaticsPlayground/main.c
@@ -1,47 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define N 5
 
-int len(const int *arr) {
-    int arrLen = sizeof(arr);
-    if (arrLen == 0) {
-        return 0;
+int arrSum(const int *arr, int arrLen) {
+    int s = 0;
+    for (int i = 0; i < arrLen; ++i) {
+        s = s + arr[i];
     }
-    int el = sizeof(arr[0]);
-    return arrLen / el;
+    return s;
 }
 
 
 int main(void) {
-    srand(time(0));
-    for (int i = 1; i <= 50; ++i) {
-        printf("%5d", rand() % 50);
+
+    char *str = "Hello world";
+    int l = strlen(str);
+
+    printf("%d\n", l);
+
+    for (; *str != '\0'; str++) {
+        printf("%c\n", *str);
+    }
+
+    int* x = (int*) malloc(sizeof(int) * 16);
+
+    for (int i = 0; i < 11; ++i) {
+        x[i] = i * i;
+    }
+
+    int a = sizeof(x);
+    printf("%d\n", *x);
+
+    for (int i = 1; i < 101; ++i) {
+        printf("%10d", x[i]);
         if (i % 10 == 0) {
             printf("\n");
         }
     }
 
-//    int arrI[N];
-//
-//    for (int i = 0; i < N; ++i) {
-//        printf("%p\n", &arrI[i]);
-//
-//    }
+    free(x);
 
-    int arrSize = 10;
-    int array[arrSize];
-    int *fourthEl = &array[3];
+    double y[5] = {1.1, 2.2, 3.3, 4.4, 0.05};
 
-    for (int i = 0; i < 3; i++) {
-        printf("%p\n", fourthEl + i);
+    for (int i = 0; i < 5; ++i) {
+        printf("%d - %f", i, y[i]);
     }
 
-    printf("%lu\n", sizeof(array));
-    printf("%lu\n", sizeof(fourthEl));
-
-    printf("%d\n", len(array));
-
     return 0;
+
+    // p. 66, 4.3.2 Структуры
 }
