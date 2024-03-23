4d8d318b1252af1d5a4af2c463f3450ef7344454
Alexey Shesterin <xenophorus@gmail.com>
2024-03-24 00:38:18 +1000

    1

--- informaticsPlayground/main.c
+++ informaticsPlayground/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
 
 int lessThanSqIdx(const int *arr, int arrSize) {
+    /* The function counts the number of array elements whose values
+     * are not greater than the values of the squares of their indices
+     *
+     * @arr int Pointer to the first element of the array
+     * @arrSize int Number of array elements
+     * @return int The number of array elements by condition
+     * */
+
     int counter = 0;
 
     for (int i = 0; i < arrSize; i++) {
@@ -12,6 +21,16 @@ int lessThanSqIdx(const int *arr, int arrSize) {
 }
 
 long productBoundaryNegs(const int *arr, int arrSize) {
+    /* If the number of negative array elements is greater than two,
+     * the function returns the product of the elements
+     * between the first and last negative elements
+     *
+     * @arr int Pointer to the first element of the array
+     * @arrSize int Number of array elements
+     * @return long The product of the elements between the first and last negative elements
+     * or zero
+     * */
+
     int fstNegIdx = -1, lastNegIdx = -1;
     long result = 1;
     for (int i = 0; i < arrSize; i++) {
@@ -29,29 +48,48 @@ long productBoundaryNegs(const int *arr, int arrSize) {
     return result;
 }
 
-void swapZeroMax(int *arr, int arrSize) {
+int* swapZeroMax(const int *arr, int arrSize) {
+    /* Function swaps the first zero and the max values of the array
+     *
+     * @arr int Pointer to the first element of the array
+     * @arrSize int Number of array elements
+     * @return int* The pointer to the first element of the array
+     * */
+
+    int *tmpArray = (int*) malloc(sizeof(int) * arrSize);
     int lastZeroIdx = -1;
     int firstMaxIdx = -1;
     int maxValue = INT_MIN;
     int tmpValue;
+
     for (int i = 0; i < arrSize; i++) {
-        if (maxValue < arr[i]) {
-            maxValue = arr[i];
+        tmpArray[i] = arr[i];
+    }
+
+    for (int i = 0; i < arrSize; i++) {
+        if (maxValue < tmpArray[i]) {
+            maxValue = tmpArray[i];
             firstMaxIdx = i;
         }
-        if (arr[i] == 0) {
+        if (tmpArray[i] == 0) {
             lastZeroIdx = i;
         }
     }
+
     if (lastZeroIdx >= 0) {
-        tmpValue = arr[lastZeroIdx];
-        arr[lastZeroIdx] = arr[firstMaxIdx];
-        arr[firstMaxIdx] = tmpValue;
-        printf("Swapped %d and %d values\n", lastZeroIdx, firstMaxIdx);
-    } else {
-        printf("No values to swap\n");
+        tmpValue = tmpArray[lastZeroIdx];
+        tmpArray[lastZeroIdx] = tmpArray[firstMaxIdx];
+        tmpArray[firstMaxIdx] = tmpValue;
     }
 
+    return tmpArray;
+}
+
+void printArray(const int *arr, int arraySize) {
+    for (int i = 0; i < arraySize; ++i) {
+        printf("%-5d", arr[i]);
+    }
+    printf("\n");
 }
 
 void test();
@@ -59,27 +97,34 @@ void test();
 int main(void) {
 
     printf("Enter array size: \n");
-
     int arrSize;
     scanf("%d", &arrSize);
 
     int arr[arrSize];
-
     printf("Enter array values: \n");
     for (int i = 0; i < arrSize; i++) {
         scanf("%d", &arr[i]);
     }
 
     int lessThanSqIdxCount = lessThanSqIdx(arr, arrSize);
+    printf("Number of array elements whose values are not greater than the square of their indices:\n");
+    printf("%d\n\n", lessThanSqIdxCount);
+
     long productBoundaryNegsVal = productBoundaryNegs(arr, arrSize);
+    printf("Product of array elements between the first and last negative elements: \n");
+    printf("%ld\n\n", productBoundaryNegsVal);
 
-    printf("%d\n", lessThanSqIdxCount);
-    printf("%ld\n", productBoundaryNegsVal);
+    int* newArray = swapZeroMax(arr, arrSize);
 
-    swapZeroMax(arr, arrSize);
+    printf("Arrays after swapping array elements: \n");
+    printf("Initial array: \n");
+    printArray(arr, arrSize);
+    printf("Resulting array: \n");
+    printArray(newArray, arrSize);
+
+    free(newArray);
 
 //    test();
-
     return 0;
 }
 
@@ -100,13 +145,15 @@ void test() {
     productBoundaryNegs(arr3, 9) == -255 ? printf("%s\n", "true") : printf("%s\n", "false");
 
     printf("%s\n", "productBoundaryNegs");
-    swapZeroMax(arr1, 9);
-    printf("%d, %d\n", 4, 8);
+    int *testArray1 = swapZeroMax(arr1, 9);
+    printArray(arr1, 9);
+    printArray(testArray1, 9);
     swapZeroMax(arr2, 9);
-    printf("%d, %d\n", 5, 8);
+    int *testArray2 = swapZeroMax(arr2, 9);
+    printArray(arr2, 9);
+    printArray(testArray2, 9);
     swapZeroMax(arr3, 9);
-    printf("No swaps\n");
-
-    printf("%s", "");
-
+    int *testArray3 = swapZeroMax(arr3, 9);
+    printArray(arr3, 9);
+    printArray(testArray3, 9);
 }
