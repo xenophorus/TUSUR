632a6de3483eaadaf5b84c56360b1c0a8890e90e
Alexey Shesterin <xenophorus@gmail.com>
2025-01-28 04:32:03 +0000

    Update labWork.sh

--- course2_Выс_системы_lab1/labWork.sh
+++ course2_Выс_системы_lab1/labWork.sh
@@ -1,79 +1,66 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "laba started"
+tempFile=./tmpfile
+touch $tempFile
 
-#getting file
+echo Введите имя файла:
+read -r workFile
 
-workfile=$1
-exitVar=""
-
-tfile=./tmpfile
-touch $tfile
-
-while [ ! -f $workfile ] 
-    do
-    echo Файл $workfile не найден!
-    echo Введите имя файла:
-    read workfile
-    done
-
-
-
-#work with file
-
-while [ ! exitVar = "exit" ]
-    do
-    lines=`cat $workfile | wc -l`
-    echo ""
-    echo ""
-    echo Работаем с файлом $workfile, количество строк - $lines.
-    echo Выберите нужный вариант и введите его номер:
-    echo 1. Вывод файла
-    echo 2. Удалить строку из файла по номеру
-    echo 3. Дописать данные в файл
-    echo 4. Выход
-
-
-    read editCommand
-
-    if [ $editCommand -eq 1 ]
+while true
+  do
+    if [ -f $workFile ]
     then
-        cat -n $workfile
-    elif [ $editCommand -eq 2 ]
-    then
-        while [[ ! $lineToRemove =~ ^([0-9]+)$ ]]
-        do
-            echo Введите номер строки:
-            read lineToRemove
-            if [ $lineToRemove -le $lines ] 
-            then
-                sed -e ${lineToRemove}d $workfile > $tfile
-                cat $tfile > $workfile
-                echo Строка ${lineToRemove} удалена
-            else
-                echo Номер строки не может быть больше строк в файле!
-            fi
-        done
-        lineToRemove=""
-    elif [ $editCommand -eq 3 ]
-    then
-        echo Введите строку для записи в ${workfile}:
-        read lineToAdd
-        echo $lineToAdd >> $workfile
+      break
     else
-        break
+      echo Файл $workFile не найден!
+      echo Введите корректное имя файла:
+      read -r workFile
     fi
+  done
+
+while true
+  do
+  lines=$(cat $workFile | wc -l)
+  echo ""
+  echo Работаем с файлом $workFile, количество строк - $lines.
+  echo Выберите нужный вариант и введите его номер:
+  echo 1. Вывод файла
+  echo 2. Удалить строку из файла по номеру
+  echo 3. Дописать данные в файл
+  echo 4. Выход
+
+  read -r editCommand
+
+  if [ $editCommand -eq 1 ]
+  then
+    cat -n $workFile
+  elif [ $editCommand -eq 2 ]
+  then
+    while [[ ! $lineToRemove =~ ^([0-9]+)$ ]]
+    do
+      echo Введите номер строки:
+      read -r lineToRemove
+      if [ $lineToRemove -le $lines ]
+      then
+        sed -e ${lineToRemove}d $workFile > $tempFile
+        cat $tempFile > $workFile
+        echo Строка ${lineToRemove} удалена
+      else
+        echo Номер строки не может быть больше строк в файле!
+      fi
     done
+    lineToRemove=""
+  elif [ $editCommand -eq 3 ]
+  then
+    echo Введите строку для записи в ${workFile}:
+    read -r lineToAdd
+    echo $lineToAdd >> $workFile
+  elif [ $editCommand -eq 4 ]
+  then
+    break
+  else
+    continue
+  fi
+  done
 
-rm $tfile
-
-
-var1=5
-while [ $var1 -gt 0 ]
-do
-echo $var1
-
-#var1=$[ $var1-1 ]
-var1=$[ $var1 - 1 ]
-
-done
+rm $tempFile
